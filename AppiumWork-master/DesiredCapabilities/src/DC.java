7f2adcdaafaab1c6bd9960807bba549252c7a9a9
anooptajawal <anoop.nair@tajawal.com>
2018-05-03 18:37:05 +0400

    added noreset capability

--- AppiumWork-master/DesiredCapabilities/src/DC.java
+++ AppiumWork-master/DesiredCapabilities/src/DC.java
@@ -1,9 +1,11 @@
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class DC {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setCapability("platforName", "Android");
 	}
 
 }

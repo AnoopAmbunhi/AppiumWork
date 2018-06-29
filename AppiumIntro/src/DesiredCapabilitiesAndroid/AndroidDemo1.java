812e57ac99e2c0668c48477a022f3fa6434ecf55
anooptajawal <anoop.nair@tajawal.com>
2018-06-29 18:22:39 +0400

    made android DC Dynamic

---
+++ AppiumIntro/src/DesiredCapabilitiesAndroid/AndroidDemo1.java
@@ -0,0 +1,15 @@
+package DesiredCapabilitiesAndroid;
+
+import java.net.MalformedURLException;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class AndroidDemo1 extends baseDesired {
+
+	public static void main(String[] args) throws MalformedURLException {
+		// TODO Auto-generated method stub
+		AndroidDriver<AndroidElement> driver = baseDesired.AndroidCapabilities();
+	}
+
+}
